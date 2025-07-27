7791c2b5972f90cbb2595fc9e0fa1f874eae8c37
Bjk8kds <58904384+Bjk8kds@users.noreply.github.com>
2025-07-27 11:57:53 +0700

    Update sensor.py

--- components/zh03b/sensor.py
+++ components/zh03b/sensor.py
@@ -1,81 +1,53 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import (
-    CONF_ID,
-    CONF_PM_2_5,
-    CONF_PM_10_0,
-    CONF_UPDATE_INTERVAL,
-    DEVICE_CLASS_PM25,
-    DEVICE_CLASS_PM10,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_MICROGRAMS_PER_CUBIC_METER,
-)
+from esphome.const import CONF_ID, UNIT_MICROGRAMS_PER_CUBIC_METER, ICON_CHEMICAL_WEAPON
+
+zh03b_ns = cg.esphome_ns.namespace('zh03b')
+ZH03BSensor = zh03b_ns.class_('ZH03BSensor', cg.Component, uart.UARTDevice)
 
 CONF_PM_1_0 = "pm_1_0"
+CONF_PM_2_5 = "pm_2_5"
+CONF_PM_10_0 = "pm_10_0"
 CONF_MODE = "mode"
-DEVICE_CLASS_PM1 = "pm1"
 
-DEPENDENCIES = ["uart"]
-
-zh03b_ns = cg.esphome_ns.namespace("zh03b")
-ZH03BSensor = zh03b_ns.class_("ZH03BSensor", cg.Component, uart.UARTDevice)
 ZH03BMode = zh03b_ns.enum("ZH03BMode")
 
-MODES = {
-    "PASSIVE": ZH03BMode.MODE_PASSIVE,
-    "QA": ZH03BMode.MODE_QA,
-}
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(ZH03BSensor),
-            cv.Optional(CONF_MODE, default="PASSIVE"): cv.enum(MODES, upper=True),
-            cv.Optional(CONF_PM_1_0): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
-                icon="mdi:chemical-weapon",
-                accuracy_decimals=0,
-                device_class=DEVICE_CLASS_PM1,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_PM_2_5): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
-                icon="mdi:chemical-weapon",
-                accuracy_decimals=0,
-                device_class=DEVICE_CLASS_PM25,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_PM_10_0): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
-                icon="mdi:chemical-weapon",
-                accuracy_decimals=0,
-                device_class=DEVICE_CLASS_PM10,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(uart.UART_DEVICE_SCHEMA)
-)
-
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ZH03BSensor),
+    cv.Optional(CONF_PM_1_0): sensor.sensor_schema(
+        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+        icon=ICON_CHEMICAL_WEAPON,
+        accuracy_decimals=0,
+    ),
+    cv.Optional(CONF_PM_2_5): sensor.sensor_schema(
+        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+        icon=ICON_CHEMICAL_WEAPON,
+        accuracy_decimals=0,
+    ),
+    cv.Optional(CONF_PM_10_0): sensor.sensor_schema(
+        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+        icon=ICON_CHEMICAL_WEAPON,
+        accuracy_decimals=0,
+    ),
+    cv.Optional(CONF_MODE, default='passive'): cv.one_of('passive', 'qa', lower=True),
+}).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
-
-    # Set mode
-    cg.add(var.set_mode(config[CONF_MODE]))
-
     if CONF_PM_1_0 in config:
         sens = await sensor.new_sensor(config[CONF_PM_1_0])
         cg.add(var.set_pm_1_0_sensor(sens))
-
     if CONF_PM_2_5 in config:
         sens = await sensor.new_sensor(config[CONF_PM_2_5])
         cg.add(var.set_pm_2_5_sensor(sens))
-
     if CONF_PM_10_0 in config:
         sens = await sensor.new_sensor(config[CONF_PM_10_0])
         cg.add(var.set_pm_10_0_sensor(sens))
+    # Set mode
+    if config[CONF_MODE] == 'qa':
+        cg.add(var.set_mode(ZH03BMode.MODE_QA))
+    else:
+        cg.add(var.set_mode(ZH03BMode.MODE_PASSIVE))
